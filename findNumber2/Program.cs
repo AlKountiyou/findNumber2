523ccc4f430ecf34424293a345d591cccda72f68
AlKountiyou <64207917+AlKountiyou@users.noreply.github.com>
2022-11-18 17:46:41 +0000

    Add files via upload

---
+++ findNumber2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+
+int number = 0;
+ArrayList choix = new ArrayList();
+int i = 0;
+entrerNombre:
+    Console.WriteLine("Entrer un nombre compris entre 1 et 10:");
+    string stringNumber = Console.ReadLine();
+
+if(!int.TryParse(stringNumber, out number))
+{
+    choix.Add(stringNumber);
+    goto entrerNombre;
+}
+else
+{
+    if(number < 1 || number > 10)
+    {
+        choix.Add(number);
+        goto entrerNombre;
+    }
+}
+if (number == 5)
+{
+    Console.WriteLine("Vous avez gagné");
+    Console.WriteLine("Tentatives: ");
+    foreach(var nbr in choix)
+    {
+        Console.Write(nbr + " ");
+    }
+}
+else
+{
+    Console.WriteLine("c'est pas bon!");
+    choix.Add(number);
+    goto entrerNombre;
+}
+
